4b97e16fdf83e2ce8b9a41bd911f8451fd72078e
JiyoonJeong-Archive <109908679+JiyoonJeong-Archive@users.noreply.github.com>
2023-10-02 18:08:57 +0900

    add linkedin

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231002.144112';
+const cacheName = 'chirpy-20231002.180701';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
