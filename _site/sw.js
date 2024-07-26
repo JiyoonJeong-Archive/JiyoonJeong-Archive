f391b8d98e879731a81afa008962396c23ea0cc5
JiyoonJeong-Archive <109908679+JiyoonJeong-Archive@users.noreply.github.com>
2024-07-26 20:50:12 +0900

    update 240726 - config.yml - exclude site2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240726.204003';
+const cacheName = 'chirpy-20240726.204630';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
